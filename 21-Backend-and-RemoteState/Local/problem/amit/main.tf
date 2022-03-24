b154d54b159c037fe24c631d70087b90b93d6207
Amit <amitvashist7@outlook.com>
2022-03-24 11:48:04 +0000

    20-External-Modules

---
+++ 21-Backend-and-RemoteState/Local/problem/amit/main.tf
@@ -0,0 +1,11 @@
+# Create a Resource Group
+resource "azurerm_resource_group" "blue" {
+  count    = 1 
+  name     = "red-RG-${count.index +1}"
+  location = "eastus"
+  tags = {
+    Owner = "Amit Vashist" 
+    Team  = "DevOps Team"
+    Project = "Terraform Automation"
+  }
+}
---
+++ 21-Backend-and-RemoteState/Local/solution/vashist/main.tf
@@ -0,0 +1,11 @@
+# Create a Resource Group
+resource "azurerm_resource_group" "blue" {
+  count    = 1 
+  name     = "Apple-RG-${count.index +1}"
+  location = "eastus"
+  tags = {
+    Owner = "Amit Vashist" 
+    Team  = "DevOps Team"
+    Project = "Terraform Automation"
+  }
+}
